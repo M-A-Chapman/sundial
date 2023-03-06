9291b3ec1773b243247cc611fbb8a71d01012aa3
Mike <64697794+M-Chapman@users.noreply.github.com>
2023-03-06 15:31:50 +0000

    changed sort method and removed await on sync ops

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 const express = require('express');
-const { batchGet, getEarliest } = require('./sunrise');
+const { getSunrises, getEarliest } = require('./sunrise');
 
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    let response;
-    batchGet(5, 100)
+    getSunrises(5, 100)
         .then((responses) => {
             const earliestSunrise = getEarliest(responses);
-            response = { earliestSunrise, responses };
+            const response = { earliestSunrise, responses };
             res.status(200).send(response);
         }).catch((err) => {
             res.status(501).send(err);
--- sunrise.js
+++ sunrise.js
@@ -1,20 +1,18 @@
 const axios = require('axios');
 
-async function randomFloat(min, max, decimals) {
+function randomFloat(min, max, decimals) {
     return (Math.random() * (max - min + 1) + min).toFixed(decimals);
 }
 
-async function getSunTimes() {
-    const [latitude, longitude] = await Promise.all([
-        randomFloat(-90, 90, 7),
-        randomFloat(-180, 180, 7),
-    ]);
+async function getSunTime() {
+    const latitude = randomFloat(-90, 90, 7);
+    const longitude = randomFloat(-180, 180, 7);
     const { data } = await axios.get(`https://api.sunrise-sunset.org/json?lat=${latitude}&lng=${longitude}`);
     const { results } = data;
     return { coordinates: { latitude, longitude }, times: { ...results } };
 }
 
-async function batchGet(limit = 5, responses = 10) {
+async function getSunrises(limit = 5, responses = 10) {
     const activeTasks = [];
     const results = [];
     for (let i = 0; i < responses; i += 1) {
@@ -22,7 +20,7 @@ async function batchGet(limit = 5, responses = 10) {
             // eslint-disable-next-line no-await-in-loop
             await Promise.race(activeTasks);
         }
-        const activeTask = getSunTimes()
+        const activeTask = getSunTime()
             // eslint-disable-next-line no-loop-func
             .then((response) => {
                 activeTasks.splice(activeTasks.indexOf(activeTask), 1);
@@ -37,57 +35,33 @@ async function batchGet(limit = 5, responses = 10) {
     return results;
 }
 
-function convertTime12to24(time12h) {
-    const [time, modifier] = time12h.split(' ');
-    let [hours, minutes, seconds] = time.split(':');
-    hours = parseInt(hours, 10);
-    minutes = parseInt(minutes, 10);
-    seconds = parseInt(seconds, 10);
-    if (hours === 12) {
-        hours = 0;
-    }
-    if (modifier === 'PM') {
-        hours = parseInt(hours, 10) + 12;
-    }
-    return [hours, minutes, seconds];
-}
-
 function getEarliest(data) {
     let earliestSunrise;
     let dayLength;
-    let tempHours = 24;
-    let tempMinutes = 60;
-    let tempSeconds = 60;
+    // default to 1 day in seconds
+    let earliestTime = 86400;
 
-    data.forEach((sunTime) => {
-        let earlierTime = false;
-        const [hours, minutes, seconds] = convertTime12to24(sunTime.times.sunrise);
-
-        if (hours < tempHours) {
-            earlierTime = true;
-        } else if (hours === tempHours) {
-            if (minutes < tempMinutes) {
-                earlierTime = true;
-            } else if (minutes === tempMinutes) {
-                if (seconds < tempSeconds) {
-                    earlierTime = true;
-                }
-            }
-        }
-
-        if (earlierTime) {
-            earliestSunrise = sunTime.times.sunrise;
-            dayLength = sunTime.times.day_length;
-            tempHours = hours;
-            tempMinutes = minutes;
-            tempSeconds = seconds;
+    data.forEach((sunTimeResponse) => {
+        const { sunrise } = sunTimeResponse.times;
+        const [time, periodOfDay] = sunrise.split(' ');
+        let [hours] = time.split(':');
+        const [, minutes, seconds] = time.split(':');
+        hours = parseInt(hours, 10) === 12 && periodOfDay === 'AM' ? 0 : parseInt(hours, 10);
+        const timeFromPeriod = periodOfDay === 'PM' ? 43200 : 0;
+        const timeInSeconds = parseInt(seconds, 10)
+            + (parseInt(minutes, 10) * 60)
+            + (hours * 3600)
+            + timeFromPeriod;
+        if (timeInSeconds < earliestTime) {
+            earliestTime = timeInSeconds;
+            earliestSunrise = sunrise;
+            dayLength = sunTimeResponse.times.day_length;
         }
     });
-
     return { earliestSunrise, dayLength };
 }
 
 module.exports = {
-    batchGet,
+    getSunrises,
     getEarliest,
 };
